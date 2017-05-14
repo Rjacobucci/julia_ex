ccbfefb25953335e6012d857d9103f1674dcd4e6
Ross Jacobucci <rcjacobuc@gmail.com>
2017-05-14 09:59:14 -0700

    julia mixed effects with wisc

---
+++ wisc_mixed_julia.jl
@@ -0,0 +1,42 @@
+Pkg.installed()
+
+Pkg.add("MultivariateStats")
+Pkg.add("DataFrames")
+Pkg.add("Requests")
+
+using MultivariateStats
+
+using DataFrames
+
+Pkg.add("MixedModels")
+using MixedModels
+
+
+
+dat = readtable("/Users/RJacobucci/Documents/Github/random_code/wisc4vpe.dat",header=false,separator=' ')
+names!(dat,[:V1,:V2,:V4,:V6,:P1,:P2,:P4,:P6,:Moeducat])
+print(head(dat))
+describe(dat)
+dat[:id] = linspace(1,204,204)
+
+# reshape
+dat2 = dat[:,[:id,:V1,:V2,:V4,:V6]]
+#wisc_stack = stack(dat,[:id])
+d = melt(dat2,[:id])
+names!(d,[:Grade,:Verbal,:id])
+
+d[:Grade2] = 1
+d[205:408,[:Grade2]] = 2
+d[205:612,[:Grade2]] = 4
+d[613:816,[:Grade2]] = 6
+
+
+
+# run mixed effects model
+
+fm1 = fit!(lmm(Verbal ~ Grade2 + (1|id),d))
+print(coef(fm1))
+print(fixef(fm1))
+print(ranef(fm1))
+print(deviance(fm1))
+print(objective(fm1))
